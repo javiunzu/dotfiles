d180eb6d17be5f6a073ab58583d21e0a51792a44
javiunzu <javiunzu@gmail.com>
2020-07-15 21:13:52 +0200

    Removed bottom bar. Use battery widgets only if there is a battery present.

--- .config/qtile/widgets.py
+++ .config/qtile/widgets.py
@@ -17,35 +17,30 @@ def glyph_forward(**kwargs):
 def glyph_backward(**kwargs):
     return widget.TextBox(text='\ue0b2', margin=0, padding=0, fontsize=20, **kwargs)
 
-def top():
-    return [
-        glyph_backward(foreground=theme['colors']['color6']),
-        widget.GroupBox(margin_x=0, background=theme['colors']['color6'], foreground=theme['colors']['color8'], this_current_screen_border=theme['colors']['color4'], highlight_method='block', rounded=False, use_mouse_wheel=False, center_aligned=True),   # selected color 4, text 8
-        glyph_forward(background='000000', foreground=theme['colors']['color6']),
-        widget.WindowName(),
-        glyph_backward(background='000000', foreground=theme['colors']['color2']),
-        widget.CurrentLayoutIcon(background=theme['colors']['color2']),
-        widget.CurrentLayout(background=theme['colors']['color2']),
-        glyph_forward(background=theme['colors']['color5'], foreground=theme['colors']['color2']),
-        widget.Volume(emoji=True, background=theme['colors']['color5']),
-        widget.Volume(emoji=False, background=theme['colors']['color5']),
-        glyph_forward(background=theme['colors']['color3'], foreground=theme['colors']['color5']),
-        widget.TextBox(text='\uf0e7', background=theme['colors']['color3']),
-        widget.Battery(background=theme['colors']['color3'], format='{char} {percent:2.0%}'),
-        widget.Systray(background=theme['colors']['color3']),
-        widget.Clipboard(background=theme['colors']['color3']),
-        glyph_forward(background='000000', foreground=theme['colors']['color3']),
-        widget.TextBox(text='\uf073'),
-        widget.Clock(format='%a %H:%M %Y-%m-%d')
-        ]
+def battery(**kwargs):
+    """ If there is a battery, add the proper widgets, otherwise just the termination glyph. """
+    return [glyph_forward(background=theme['colors']['color3'], foreground=theme['colors']['color5']),
+            widget.TextBox(text='\uf0e7', background=theme['colors']['color3']),
+            widget.Battery(background=theme['colors']['color3'], format='{char} {percent:2.0%}'),
+            glyph_forward(background='000000', foreground=theme['colors']['color3'])] if os.listdir('/sys/class/power_supply') else [glyph_forward(background='000000', foreground=theme['colors']['color5'])]
 
-def bottom():
-    return [
-        widget.Prompt(),
-        widget.TaskList(),
-        widget.Notify(background=theme['colors']['color3']),
-        glyph_backward(background='000000', foreground=theme['colors']['color4']),
-        widget.CheckUpdates(background=theme['colors']['color4'], restart_indicator='⭮'),
-        glyph_forward(background='000000', foreground=theme['colors']['color4']),
-        widget.Net(interface='wlp4s0'),
-        ]
+def top():
+    widgets = [widget.Sep(padding=10, foreground='000000'),
+               glyph_backward(foreground=theme['colors']['color6']),
+               widget.GroupBox(margin_x=0, background=theme['colors']['color6'], foreground=theme['colors']['color8'], this_current_screen_border=theme['colors']['color4'], highlight_method='block', rounded=False, use_mouse_wheel=False, center_aligned=True),   # selected color 4, text 8
+               glyph_forward(background='000000', foreground=theme['colors']['color6']),
+               widget.WindowName(),
+               glyph_backward(background='000000', foreground=theme['colors']['color2']),
+               widget.CurrentLayoutIcon(background=theme['colors']['color2']),
+               glyph_forward(background=theme['colors']['color5'], foreground=theme['colors']['color2']),
+               widget.Volume(emoji=True, background=theme['colors']['color5']),
+               widget.Volume(emoji=False, background=theme['colors']['color5'])]
+    widgets.extend(battery())
+    widgets.extend([widget.Sep(padding=10, foreground='000000'),
+                    widget.Sep(padding=10, foreground='000000'),
+                    widget.Systray(),
+                    widget.Clipboard(background=theme['colors']['color3']),
+                    widget.TextBox(text='\uf073'),
+                    widget.Clock(format='%a %H:%M %Y-%m-%d')])
+    return widgets
+
